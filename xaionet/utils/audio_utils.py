6acdfc219416800fac04d5608534e6dbc1c511a8
Varsh_01 <varshah.cm@gmail.com>
2025-10-02 11:48:04 +0530

    Add files via upload

--- xaionet/utils/audio_utils.py
+++ xaionet/utils/audio_utils.py
@@ -1,1 +1,12 @@
+# utils/audio_utils.py
+import io
+import soundfile as sf
+
+def wav_bytes_from_array(np_audio, samplerate=16000):
+    """
+    Convert numpy array to WAV bytes (mono).
+    """
+    buf = io.BytesIO()
+    sf.write(buf, np_audio, samplerate, format='WAV')
+    return buf.getvalue()
 
